a567eb714cd78ec31570dee9a984e846f8a8955a
Ana Dal Moro <105503787+AnaDalMoro@users.noreply.github.com>
2024-04-30 10:46:44 -0300

    tufri

--- main.js
+++ main.js
@@ -18,6 +18,16 @@ const tempoObjetivo2 = new Date("2024-12-31T23:59:59");
 const tempoObjetivo3 = new Date("2024-12-13T23:59:59");
 const tempoObjetivo4 = new Date("2024-12-14T23:59:59");
 
+const tempos = [
+    tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+]
+
+for(let i=0; i< contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
 function calculaTempo(tempoObjetivo1){
     let tempoAtual = new Date();
     contadores[0].textContent = tempoObjetivo1 - tempoAtual;
